0d7934dfcebf5dd3df42eafe258a2775d3bbb292
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2022-05-30 17:46:50 +0530

    Stringify resp

--- functions/counter_function/index.js
+++ functions/counter_function/index.js
@@ -10,15 +10,21 @@ module.exports = (req, res) => {
 	if (url === '/visitors' && req.mode === 'GET') {
 		incrementVisitors();
 		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.write({
+		res.write(JSON.stringify({
 			'visitors' : numberOfVisitors
-		});
+		}));
+		return;
 	} else if (url === '/visitors' && req.mode === 'PUT') {
 		let secretKey = req.query.secretKey;
 		if (secretKey === 'sudo') {
 			let newCount = req.query.newCount;
 			numberOfVisitors = newCount;
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write(JSON.stringify({
+				'visitors' : numberOfVisitors
+			}));
 		}
+		return;
 	}
 	
 	res.writeHead(404, { 'Content-Type': 'application/json' });
